9e4d0f925f0cf6205cbf1f876344d9eb9ae9e804
DESKTOP <hanyong835@gmail.com>
2023-09-19 23:25:53 +0900

    PC230919

--- src/component/pages/config/index.js
+++ src/component/pages/config/index.js
@@ -1,8 +1,29 @@
+import { Button, Form, Input } from "antd";
+
 function Config() {
+  const [form] = Form.useForm();
+  const formItemLayout = { labelCol: { span: 4 }, wrapperCol: { span: 14 } };
+
   return (
-    <div>
-      <p>CONFIG</p>
-    </div>
+    <Form
+      {...formItemLayout}
+      layout="vertical"
+      form={form}
+      style={{ margin: "20px" }}
+    >
+      <Form.Item label="HOST NAME">
+        <Input placeholder="HOST NAME" />
+      </Form.Item>
+      <Form.Item label="IP">
+        <Input placeholder="IP" />
+      </Form.Item>
+      <Form.Item label="ROOT">
+        <Input placeholder="ROOT" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary">ASIGN</Button>
+      </Form.Item>
+    </Form>
   );
 }
 
